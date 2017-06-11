d79ba8acd93259451d50000904c4fff11a4ebc08
Robert Ioffe <robert.ioffe@intel.com>
2017-06-11 12:02:20 -0700

    added lag

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -289,6 +289,6 @@ vector<double> MPC::Solve(Eigen::VectorXd x0, Eigen::VectorXd coeffs) {
           solution.x[x_start + 24],   solution.x[y_start + 24],
           solution.x[psi_start + 1], solution.x[v_start + 1],
           solution.x[cte_start + 1], solution.x[epsi_start + 1],
-          solution.x[delta_start],   solution.x[a_start]};
+          solution.x[delta_start+2],   solution.x[a_start+2]};
 }
 
